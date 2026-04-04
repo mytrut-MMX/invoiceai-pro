43ebc61f56f4259fbb7e12964de6ff98521b139c
mytrut-MMX <mytrut@gmail.com>
2026-04-04 11:19:52 +0100

    Update product-workflow-lead.js

--- api/product-workflow-lead.js
+++ api/product-workflow-lead.js
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
-import { PRODUCT_WORKFLOW_LEAD_PROMPT } from "../lib/prompts/productWorkflowLead";
+import { PRODUCT_WORKFLOW_LEAD_PROMPT } from "../lib/prompts/productWorkflowLead.js";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
